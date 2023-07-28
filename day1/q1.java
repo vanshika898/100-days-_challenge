b4357566891381c09474eb69371d83a4b63305f1
vanshika898 <140736318+vanshika898@users.noreply.github.com>
2023-07-28 19:34:51 +0000

    main

---
+++ day1/q1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class day2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter the Second number:");
+        int b = sc.nextInt();
+        System.out.println("Enter the third number:");
+        int c = sc.nextInt();
+        int average = (a + b + c) / 3;
+        System.out.printf("The average of three numbers %d, %d, %d is %d", a, b, c, average);
+    }
+}
+
+
+
+
+
+
+
