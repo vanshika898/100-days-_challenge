c9cdaa6cdfa6d32afa16af24c2d44ed7764fa242
vanshika898 <140736318+vanshika898@users.noreply.github.com>
2023-08-03 14:46:55 +0000

    main

---
+++ day2(operators)/q5.java
@@ -0,0 +1,12 @@
+public class q5 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println("add = " + (a + b));
+        System.out.println("difference = " + (a - b));
+        System.out.println("multiply = " + (a * b));
+        System.out.println("divide = " + (a / b));
+        System.out.println("remainder = " + (a % b));
+    }
+}
+
