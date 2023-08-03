0ed12cb26a884cf7d341bafdb28713f4428316c7
vanshika898 <140736318+vanshika898@users.noreply.github.com>
2023-08-03 15:53:58 +0000

    main

---
+++ day2/q6.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class q6{
+    public static void main (String[]args){
+    int age = 18;
+    if (age==18){
+        System.out.println("you are now adult");
+    }
+    else{
+        System.out.println("you are either bigger than 18 or smaller than 18");
+    }
+    }}
+
