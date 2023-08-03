d0da459179a98c6bd3901042840a8e86c1290061
vanshika898 <140736318+vanshika898@users.noreply.github.com>
2023-08-03 16:58:33 +0000

    main

---
+++ day2/q9.java
@@ -0,0 +1,16 @@
+class q9{
+    public static void main(String[]args){//code for largest from three numbers
+        int a=1;
+        int b=2;
+        int c=3;
+        if (a>b && a>c){
+            System.out.println("largest number is a");}
+        else if (b>a && b>c){
+            System.out.println("largest number is b");}
+        else if (c>a && c>b){
+            System.out.println("largest number is c"); } 
+        else{
+            System.out.println("all number are equal");        
+        }    
+        }
+    }
