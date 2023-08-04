43b0db41a1d76f1d3e08445928060fc280d40ac8
vanshika898 <140736318+vanshika898@users.noreply.github.com>
2023-08-04 17:30:56 +0000

    main

---
+++ day3/q11.java
@@ -0,0 +1,15 @@
+class q11{
+    public static void main(String[]args){
+        int number=2;
+        switch(number){
+            case 1 : System.out.println("jalebi fafda");
+            break;
+            case 2 : System.out.println("momos");
+            break;
+            case 3 : System.out.println("pastry");
+            break;
+            default : System.out.println("chai nasta");
+
+        }
+    }
+}
