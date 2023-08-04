0289e558197bf6aa63425a1d40c48ad064066fb9
vanshika898 <140736318+vanshika898@users.noreply.github.com>
2023-08-04 18:11:36 +0000

    main

---
+++ day3/switchcase.java
@@ -0,0 +1,14 @@
+class switchcase{
+    public static void main(String[]args){
+        int day=1;
+        switch(day){
+            case 1:System.out.println("Monday");break;
+            case 2:System.out.println("Tuesday");break;
+            case 3:System.out.println("wednesday");break;
+            case 4:System.out.println("thursday");break;
+            case 5:System.out.println("friday");break;
+            case 6:System.out.println("Saturday");break;
+            default :System.out.println("Sunday");
+        }
+    }
+}
