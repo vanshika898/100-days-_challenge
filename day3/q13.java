3aa8dc72d2248fcc1b18f7976571bbdf976cc729
vanshika898 <140736318+vanshika898@users.noreply.github.com>
2023-08-04 17:56:53 +0000

    main

---
+++ day3/q13.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class q13{
+    public static void main (String []args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int a = sc.nextInt();
+        if (a>=0){
+            System.out.println("number is positive");//code for check number is positive or negative
+        }
+        else{
+            System.out.println("number is negative");
+        }
+    }
+
+}
