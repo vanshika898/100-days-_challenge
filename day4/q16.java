aca9bdc93e78dbd96ab2ec0b341fd7323cd48998
vanshika898 <140736318+vanshika898@users.noreply.github.com>
2023-08-05 17:56:37 +0000

    main

---
+++ day4/q16.java
@@ -0,0 +1,10 @@
+class q16{
+    public static void main(String[]args){
+        int counter = 0;
+        while(counter<=100){
+            System.out.println("helloworld");
+            counter++;
+        }
+    }
+    
+}
