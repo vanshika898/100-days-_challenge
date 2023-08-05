56cfffd1e13b34764825280c7b6cb8668be31fb4
vanshika898 <140736318+vanshika898@users.noreply.github.com>
2023-08-05 18:20:11 +0000

    main

---
+++ day4/q19.java
@@ -0,0 +1,11 @@
+class q19{
+    public static void main(String[]args){
+        int n=7;
+        int sum=0;
+        int i=0;
+        while(i<=n){
+            sum=sum+i;
+            i++;
+        }
+        System.out.println(sum);
+    }}
