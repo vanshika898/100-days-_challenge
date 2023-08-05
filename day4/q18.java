e2af397e7bd5e348bf1dd1bf1bdcf638fffbb305
vanshika898 <140736318+vanshika898@users.noreply.github.com>
2023-08-05 18:15:06 +0000

    main

---
+++ day4/q18.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class q18{
+    public static void main(String[]args){
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter the range");
+     int a =sc.nextInt();
+     int counter =1;
+     while(counter<=a){
+        System.out.print(counter+" ");
+        counter ++;
+     }
+     System.out.println();
+
+    }}
