ac198b67e1a3e5ac72b367137b8bef9568a46585
vanshika898 <140736318+vanshika898@users.noreply.github.com>
2023-08-06 17:31:20 +0000

    main

---
+++ day5/q3.java
@@ -0,0 +1,10 @@
+class q3{
+    // code for print the square of =
+    public static void main(String []args){
+        for(int i=0;i<=4;i++){
+            for (int j=0;j<=4;j++){
+                System.out.print("="+ " ");
+            }
+            System.out.println();
+        }
+    }}
