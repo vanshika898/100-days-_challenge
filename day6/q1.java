aaa53f04c08c30f1d3b25c316545769835abf3ab
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-08 18:23:21 +0000

    main

---
+++ day6/q1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class q1{
+    public static void main(String[]args){
+        int counter =1;
+        do{
+            System.out.println("helloworld");
+            counter++;
+
+        }while (counter<=10);
+    }
+}
