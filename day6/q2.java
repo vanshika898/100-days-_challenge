108caeb465ae6a2d3dc6ec30c1c9b4d29bdca487
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-08 18:28:23 +0000

    main

---
+++ day6/q2.java
@@ -0,0 +1,10 @@
+class q2{
+    public static void main( String []args){
+        for(int i=1;i<=6;i++){
+            System.out.println(i);
+            if(i==4){
+                break;
+            }
+        }
+    }
+}
