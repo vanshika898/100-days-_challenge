df3fda1da800480975dedec6b46b3208e65f8935
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-10 18:01:31 +0000

    main

---
+++ day7/q2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class q2{//code for break statement
+    public static void main(String[]args){
+        System.out.println("enter the numbers");
+        Scanner sc=new Scanner(System.in);
+        for(i=1;i<=6;i++){
+         int a =sc.nextInt();
+        }
+        for(int j=1;j<=a;j++){
+            if(a%10==0){
+                continue;
+            }
+            Sytem.out.println(j);
+        }
+        
+
+    }}
