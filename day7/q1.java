78220279390c0a1ef0779399ce04956077a3ee76
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-10 17:51:53 +0000

    main

---
+++ day7/q1.java
@@ -0,0 +1,11 @@
+class q1{//code for break statement
+    public static void main(String[]args){
+        for(int i=1;i<=6;i++){
+            if (i==4){
+                continue;}
+            System.out.println(i);
+            
+            
+        }
+    }
+}
