f2215b7d0454171350e02af122d931eee6bb60fd
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-19 12:13:05 +0000

    main

---
+++ day9/q3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class q3{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(a+"*"+i+"="+a*i);
+        }
+    }
+}
