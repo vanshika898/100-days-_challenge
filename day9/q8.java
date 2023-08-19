a19dd9ecc17c384e48b81948f5441fb2f1adc9ae
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-19 12:53:08 +0000

    main

---
+++ day9/q8.java
@@ -0,0 +1,13 @@
+class q8{
+    public static void main(String args[]){
+        int n=5;
+        int a=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+            System.out.print(a+" ");
+            a++;
+        }
+        System.out.println();
+        }
+    }
+}
