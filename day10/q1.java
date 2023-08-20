0879be01cca50d62732989d8ed7f1f49c307dcea
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-20 17:37:55 +0000

    main

---
+++ day10/q1.java
@@ -0,0 +1,9 @@
+class q1{
+    public static void hello(){//class in java
+        System.out.println("helloworld");
+
+    }
+    public static void main(String args[]){
+        hello();
+    }
+}
