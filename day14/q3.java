f8b2b9bc9a2aa9263da607b4ef3e793215102b0e
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-24 18:27:57 +0000

    main

---
+++ day14/q3.java
@@ -0,0 +1,11 @@
+// Write a Java method to compute the average of three numbers..
+class q3{
+    public static void average(int a,int b,int c){
+        float z=(float)(a+b+c)/3;
+        System.out.println("Average is: "+z);
+
+    }
+    public static void main (String[]args){
+        average(2,3,4);
+    }
+}
