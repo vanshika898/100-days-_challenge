3e59b098acd130069089d46c1344446287dd33f0
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-25 07:59:54 +0000

    main

---
+++ day15/q2.java
@@ -0,0 +1,15 @@
+class q2{
+    public static void main (String []args){
+        
+        System.out.println(Math.abs(-10));
+        System.out.println((int )Math.sqrt(4));
+        System.out.println(Math.log(2));
+        System.out.println(Math.min(2,3));
+        System.out.println(Math.max(4,5));
+        System.out.println(Math.pow(2,2));
+        
+        
+
+
+    }
+}
