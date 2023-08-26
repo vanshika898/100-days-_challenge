620a6656b222121d2786aa9d7c67f2d8e34b479b
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-26 07:37:01 +0000

    main

---
+++ day16/q1.java
@@ -0,0 +1,18 @@
+class q1{
+    public static void star(int n)
+    {
+         for (int i=1;i<=n;i++){
+           for(int j=1;j<=n-i;j++){
+               System.out.print(" ");
+           }
+           for(int j=1;j<=i;j++){
+               System.out.print("*");
+           }
+            System.out.println();
+       }
+    }
+    public static void main(String []args){
+        star(5);
+        
+    }
+}
