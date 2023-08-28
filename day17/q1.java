9f96db4adadaa79da3dd41ce1a76a9b1b43bc114
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-28 18:37:48 +0000

    main

---
+++ day17/q1.java
@@ -0,0 +1,27 @@
+class q1{
+    public static void rhombus(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(n-i);j++){
+                System.out.print("  ");
+                }
+                for(int j=1;j<=n;j++){
+                    System.out.print("*"+"");
+
+                }
+                System.out.println();
+            }
+        }
+    
+public static void main (String[]args){
+    rhombus(5);
+}
+}
+
+
+
+
+
+
+
+
+
