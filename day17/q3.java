5edc80450b638bca7b46d8baaf4ea172930e6109
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-28 19:14:15 +0000

    main

---
+++ day17/q3.java
@@ -0,0 +1,17 @@
+public class q3 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (n - i + 1); j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
