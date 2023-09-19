adcfeec1cb3235dfe9a7e63bf0b09827f21e927b
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-09-19 18:10:01 +0000

    main

---
+++ day25/diagonal_sum.java
@@ -0,0 +1,24 @@
+class diagonal_sum{
+    public static int diagonalsum(int matrix[][]){
+        int sum=0;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(i==j){
+                    sum+=matrix[i][j];
+                }
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(i+j == matrix.length-1){
+                    sum+=matrix[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {{1, 2, 3,4}, {5, 6, 7,8}, {9, 10, 11,12},{13,14,15,16}};
+        System.out.println("sum of diagonal is:"+diagonalsum(matrix));
+    }
+}
