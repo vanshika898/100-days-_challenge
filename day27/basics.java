3d88636ce889ef47ecbf8b73507aff23603f42c2
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-09-20 18:27:35 +0000

    main

---
+++ day27/basics.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class basics{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str);
+        String str4="vanshika";
+        System.out.println(str4.length());
+        String firstname="vanshika";
+        String lastname="Nigam";
+        String fullname=firstname+" "+lastname;
+        System.out.println(fullname);
+
+    }
+}
