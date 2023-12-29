61d6865ba77f52bff3b8b3e3fe6e57706ee9026a
vanshika898 <vanshikanigam8989@gmail.com>
2023-12-29 22:02:42 +0530

    day29q1

--- day29/q1.java
+++ day29/q1.java
@@ -1,6 +1,21 @@
-package day29;
+class bulb {
+    private int wattage;
 
-public class q1 {
-    int i = 1;
+    public void setWattage(int wattage) {
+        this.wattage = wattage;
+    }
 
+    public int getWattage() {
+        return this.wattage;
+    }
+}
+
+class q1 {
+
+    public static void main(String[] args) {
+        bulb m = new bulb();
+        m.setWattage(20);
+        System.out.println(m.getWattage());
+
+    }
 }
