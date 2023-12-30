9b105afeb2dd7ef7b023d5d6eca0be03aa0e96de
vanshika898 <vanshikanigam8989@gmail.com>
2023-12-30 22:07:02 +0530

    linkedlistDSA

---
+++ day29/q6.java
@@ -0,0 +1,54 @@
+
+class LinkedList {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void printlist() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + "->");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
+
+public class q6 {
+    public static void main(String[] args) {
+        LinkedList a = new LinkedList();
+        a.addFirst(20);
+        a.addFirst(30);
+        a.addLast(10);
+        a.printlist();
+    }
+}
